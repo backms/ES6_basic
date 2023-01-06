5b91e7fe7151794b9c4773add00aaaff45fd9d99
백민수 <iphonex@kakao.com>
2023-01-06 15:00:10 +0900

    베이직0 수정

--- basic0/0-3.js
+++ basic0/0-3.js
@@ -10,7 +10,7 @@ const log = console.log;
 const apply1 = f => f(1);
 const add2 = a => a + 2;
 log(apply1(add2));
-log(apply1(a => a - 1));
+log(apply1(a => a - 1));    // 0 출력 되는 이유? : f => f(1)이기 때문에 a에 1이 들어감 결국 1-1=0
 
 const times = (f, n) => {
     let i = -1;
@@ -25,6 +25,6 @@ times(a => log(a + 10), 3);
 
 const addMaker = a => b => a + b;
 const add10 = addMaker(10);
-log(add10);
-log(add10(5));
-log(add10(10));
+log(add10);     // 10 출력이 아닌 b => a + b가 출력 되는 이유 : a값을 기억하고있찌만 콘솔에는 b => a + b로 출력
+log(add10(5));  // 10 + 5 = 15 출력
+log(add10(10)); // 10 + 10 = 20 출력
